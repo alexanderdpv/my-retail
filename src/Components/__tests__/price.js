a1aae7b37ceb9ae95d5fab6aa58dd1bb2105d2e4
Alexander Della Polla Villarroel <alexander.dpv@gmail.com>
2018-06-22 01:35:09 -0500

    Implement Price component test cases using JSON input data.

---
+++ src/Components/__tests__/price.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import Price from '../Price/price';
+
+const offersData = [
+   {
+    "OfferPrice": [
+     {
+      "formattedPriceValue": "$139.99",
+      "priceQualifier": "Online Price"
+     }
+    ]
+   }
+ ];
+
+describe('Price component', () => {
+  it('Price component renders without crashing', () => {
+    const priceComponent = shallow(<Price offers={offersData}/>);
+    expect(priceComponent.find('.price').length).toEqual(1);
+  });
+
+  it('Price renders pricing data', () => {
+    const priceComponent = shallow(<Price offers={offersData}/>);
+    expect(priceComponent.find('.price-value').text()).toEqual("$139.99");
+    expect(priceComponent.find('.price-qualifier').text()).toEqual("Online Price");
+  })
+})
