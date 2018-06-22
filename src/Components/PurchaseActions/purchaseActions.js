0b21dec2585a914c134422db7b27d0ab0b178a48
Alexander Della Polla Villarroel <alexander.dpv@gmail.com>
2018-06-21 23:05:35 -0500

    Fix warnings

--- src/Components/PurchaseActions/purchaseActions.js
+++ src/Components/PurchaseActions/purchaseActions.js
@@ -16,18 +16,18 @@ class PurchaseActions extends Component {
   renderChannelButtons() {
     const { channelCode } = this.props;
 
-    if (channelCode == 0) {
+    if (channelCode === 0) {
       return (
         <div>
           <button className="purchase-primary-cta pick-up">Pick Up In Store</button>
           <button className="purchase-primary-cta add-cart">Add To Cart</button>
         </div>
       )
-    } else if (channelCode == 1) {
+    } else if (channelCode === 1) {
       return (
         <button className="purchase-primary-cta add-cart">Add To Cart</button>
       )
-    } else if (channelCode == 2) {
+    } else if (channelCode === 2) {
       return (
         <button className="purchase-primary-cta pick-up">Pick Up In Store</button>
       )
