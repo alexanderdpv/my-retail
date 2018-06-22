41240c8974d6da872192cf315eacbe4a5bef70d1
Alexander Della Polla Villarroel <alexander.dpv@gmail.com>
2018-06-21 19:17:19 -0500

    Remove the need to fetch JSON file from server, replacing with file from source.

--- src/App.js
+++ src/App.js
@@ -3,34 +3,21 @@ import ItemDescription from './Components/ItemDescription/itemDescription';
 import ItemImages from './Components/ItemImages/itemImages';
 import Reviews from './Components/Reviews/reviews';
 import ItemDetails from './Components/ItemDetails/itemDetails';
+import itemData from './item-data.json';
 import './simple-grid.min.css';
 import './App.css';
 import 'font-awesome/css/font-awesome.css';
 
 class App extends Component {
-  state = {
-    catalogItems: []
-  }
-
-  componentDidMount() {
-    fetch('http://localhost:3000/CatalogEntryView')
-      .then((response) => response.json())
-      .then((responseJSON) => {
-        this.setState({ catalogItems : responseJSON });
-      });
-  }
-
   render() {
     return (
       <div className="App container">
-        {this.state.catalogItems.map((catalogItem) => (
-          <div>
-            <div className="row">
-              <ItemImages images={catalogItem.Images} title={catalogItem.title}/>
-              <ItemDetails item={catalogItem}/>
-              <Reviews reviews={catalogItem.CustomerReview}/>
-              <ItemDescription itemDescriptions={catalogItem.ItemDescription}/>
-            </div>
+        {itemData.CatalogEntryView.map((catalogItem) => (
+          <div className="row">
+            <ItemImages images={catalogItem.Images} title={catalogItem.title}/>
+            <ItemDetails item={catalogItem}/>
+            <Reviews reviews={catalogItem.CustomerReview}/>
+            <ItemDescription itemDescriptions={catalogItem.ItemDescription}/>
           </div>
         ))}
       </div>
